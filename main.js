2d111c09550a957303b0898ae036f8b5f08d34d3
pajama10000 <99984344+pajama10000@users.noreply.github.com>
2022-10-23 13:21:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ window.addEventListener("load", () => {
             copy.style.top = "0";
             copy.setAttribute("id", "ta");
             document.body.appendChild(copy);
-            copy.textContent = "pvphub.me";
+            copy.textContent = "legacymc.net";
             copy.select();
             document.execCommand("copy");
             setTimeout(() => {
@@ -40,7 +40,7 @@ window.addEventListener("load", () => {
 })
 
 function onPlayerCountUpdate() {
-    document.getElementById("#jointoday").innerHTML = `Join ${PLAYER_COUNT} players now`
+    document.getElementById("#jointoday").innerHTML = `Join ${PLAYER_COUNT} players now on LegacyMC`
 }
 
 const updatePlayercount = (ip, port) => {
@@ -53,10 +53,10 @@ const updatePlayercount = (ip, port) => {
         }
     });
     setTimeout(() => {
-        updatePlayercount("pvphub.me", 25565);
+        updatePlayercount("legacymc.net", 25565);
     }, 30000)
 };
-updatePlayercount("pvphub.me", 25565);
+updatePlayercount("legacymc.net", 25565);
 
 $(window).scroll(function () {
 
